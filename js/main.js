4991dd534f9b97b34b995390f40ed83596067913
Raul Lopes Tomaz <105323797+RaulLTomaz@users.noreply.github.com>
2024-03-23 02:32:03 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("document").ready(function() {
+
+    console.log($("#telefone"))
+
+
+    $("#telefone").mask("(00) 00000-0000")
+
+    $("#cpf").mask("000.000.000-00")
+
+    $("#cep").mask("00.000-000")
+})
